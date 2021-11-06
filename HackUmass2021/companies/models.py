d4abe4076f0081c964476680f6b31cfeb16599ee
Isaac Gebrewolde <igebrewolde@gmail.com>
2021-11-06 01:06:36 -0400

    basic Company model

--- HackUmass2021/companies/models.py
+++ HackUmass2021/companies/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Company(models.Model):
-    pass
 
 class Graph(models.Model):
     pass
+class Company(models.Model):
+    name = models.CharField(max_length=40)
+    info = models.CharField(max_length=1000)
+    # graph = models.OneToOneField(Graph)
