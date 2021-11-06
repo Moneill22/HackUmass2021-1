3d60e7302eb827f620badbfd127cdcad90a614b4
Isaac Gebrewolde <igebrewolde@gmail.com>
2021-11-06 01:28:11 -0400

    added company form and view

--- HackUmass2021/companies/views.py
+++ HackUmass2021/companies/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .forms import CompanyForm
 
 # Create your views here.
+def Company_create_view(request):
+	form = CompanyForm(request.POST or None)
+
+	if form.is_valid():
+		form.save()
+		form = CompanyForm()
+
+	context = {
+		'form': form
+	}
+	return render(request, "form/create.html", context)
