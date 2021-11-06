57ffb3b60ffa0ba752c64eda4c3bd625c1d9a67e
Isaac Gebrewolde <igebrewolde@gmail.com>
2021-11-06 15:39:30 -0400

    updated url patterns

--- HackUmass2021/HackUmass2021/urls.py
+++ HackUmass2021/HackUmass2021/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from Users.views import User_profile_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('create/', User_profile_view)
+    path('create/', User_profile_view),
+    path('', include('FrontEnd.urls')),
 ]
