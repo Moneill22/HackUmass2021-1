2b807bf8ea454f69c43b252401bf247c94f9f579
Isaac Gebrewolde <igebrewolde@gmail.com>
2021-11-06 17:38:40 -0400

    added frontend views and urls

--- HackUmass2021/FrontEnd/urls.py
+++ HackUmass2021/FrontEnd/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('signup', views.signup)
+    path('signup', views.signup),
+    path('signin', views.signin),
+    path('dashboard/<str:user_id>', views.dashboard)
 ]
--- HackUmass2021/FrontEnd/views.py
+++ HackUmass2021/FrontEnd/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from Users.models import User, Application
+from companies.models import Company
 
 # Create your views here.
 
@@ -7,4 +9,26 @@ def index(request):
 
 def signup(request):
     return render(request, 'signup.html')
+
+def signin(request):
+    return render(request, 'signin.html')
+
+def dashboard(request, user_id):
+    # at most 3 most recent applications, at most 3 company recommendations
+    NUM_APPS = 3
+    user = User.objects.get(id=user_id)
+    applications = Application.objects.filter(user=user)
+    
+    app_context = {}
+    # rec_context = {} # TODO add company recs
+
+    for app in applications:
+        if len(app_context) == NUM_APPS:
+            break
+        app_context.update({app.company_id : app.response})
+
+
+    context = app_context # TODO add company recs
+    print(context)
+    return render(request, 'dashboard.html', context)
 
