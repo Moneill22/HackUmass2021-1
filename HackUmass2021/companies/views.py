e337226a55e6de0143271b21c119127319105a5f
Moneill22 <moneill@umass.edu>
2021-11-06 21:15:34 -0400

    tabs

--- HackUmass2021/companies/views.py
+++ HackUmass2021/companies/views.py
@@ -5,19 +5,17 @@ from .models import Company, Graph
 
 # Create your views here.
 def Company_create_view(request):
-	form = CompanyForm(request.POST or None)
-
-	if form.is_valid():
-		form.save()
-		form = CompanyForm()
-
+    form = CompanyForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = CompanyForm()
         # init graph
-        graph = Graph(company_id=form.name) 
+        graph = Graph(company_id=form.name)
         graph.save()
-	context = {
+    context = {
 		'form': form
 	}
-	return render(request, "form/create.html", context)
+    return render(request, "form/create.html", context)
 
 #Helper Functions
 def add_user_to_graph(user_id, company_name):
